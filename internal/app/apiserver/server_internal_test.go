2495e730b03f3216a63ac3334e7dd53fa3fee2a6
AveCaesar17 <tokan.yulik@gmail.com>
2022-12-29 17:46:43 +0300

    added test for /create_user

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -1,6 +1,8 @@
 package apiserver
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,9 +12,47 @@ import (
 )
 
 func TestServer_HandleUserCreate(t *testing.T) {
-	rec := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodPost, "/test", nil)
 	s := newServer(teststore.New())
-	s.ServeHTTP(rec, req)
-	assert.Equal(t, rec.Code, http.StatusOK)
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedcode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]string{
+				"username": "caesar_Test",
+				"pubkey":   "pubkey_Test",
+			},
+			expectedcode: http.StatusCreated,
+		},
+		{
+			name:         "invalid",
+			payload:      "invalid",
+			expectedcode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid_params",
+			payload: map[string]string{
+				"username": "usertecset",
+			},
+			expectedcode: http.StatusUnprocessableEntity,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			b := &bytes.Buffer{}
+			json.NewEncoder(b).Encode(tc.payload)
+			req, _ := http.NewRequest(http.MethodPost, "/create_user", b)
+			s.ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedcode, rec.Code)
+
+		})
+	}
+	// rec := httptest.NewRecorder()
+	// req, _ := http.NewRequest(http.MethodPost, "/test", nil)
+	// s := newServer(teststore.New())
+	// s.ServeHTTP(rec, req)
+	// assert.Equal(t, rec.Code, http.StatusOK)
 }
